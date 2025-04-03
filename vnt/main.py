63c4c2a63bae212c15ae34c6bca3693e7ccb1b64
Alex Santana <aleximbw@gmail.com>
2025-04-02 22:20:10 -0300

    FastAPI + Jinja2
    Aplicação do Jinja2 para a leitura de paginas HTML por parte do FastAPI

--- vnt/main.py
+++ vnt/main.py
@@ -1,7 +1,20 @@
-from fastapi import FastAPI
+#IMPORTAÇÃO DO FASTAPI COM O Request
+from fastapi import FastAPI, Request
 
+#IMPORTAÇÃO JINJA2
+from fastapi.templating import Jinja2Templates
+
+#PARTE PYTHON INSTANCIANDO O JINJA2 EM VARIÁVEL templates
+templates = Jinja2Templates(directory="templates")
+
+#INICIO DA ATIVAÇÃO DO FastAPI
 app = FastAPI()
 
+#ROTA DA PAGINA INICIAL OU RAIZ
 @app.get("/")
-def read_root():
-    return {"message" : "Servidor Rodando"}
+
+#APLICAÇÃO DO JINJA2 PARA QUE SEJA POSSÍVEL QUE O FastAPI LEIA ARQUIVOS HTML
+def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+#FINAL DA ATIVAÇÃO DO FastAPI
