8fe9ce3bf644ff1c79a0177bdbdbfe857a02a021
Alex Santana <aleximbw@gmail.com>
2025-04-03 00:05:49 -0300

    Stop no front-end
    Parado na apresentação dos dados na tela.

--- vnt/main.py
+++ vnt/main.py
@@ -4,6 +4,11 @@ from fastapi import FastAPI, Request
 #IMPORTAÇÃO JINJA2
 from fastapi.templating import Jinja2Templates
 
+#IMPORTAÇÃO DA FUNÇÃO DE CAPTURA DE REDE CRIADA POR MIM NO ARQUIVO network_monitor.py
+from network_monitor import monitor
+
+from flask import render_template
+
 #PARTE PYTHON INSTANCIANDO O JINJA2 EM VARIÁVEL templates
 templates = Jinja2Templates(directory="templates")
 
@@ -16,5 +21,15 @@ app = FastAPI()
 #APLICAÇÃO DO JINJA2 PARA QUE SEJA POSSÍVEL QUE O FastAPI LEIA ARQUIVOS HTML
 def read_root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
+@app.get("/")
+async def dados_de_rede(request: Request):
+    dados_rede = monitor()
+    return templates.TemplateResponse("index.html", {"request": request, "dados_rede": dados_rede})
+
+@app.get("/dados_rede")
+async def dados_rede():
+    dados = monitor
+    return dados
 
 #FINAL DA ATIVAÇÃO DO FastAPI
--- vnt/network_monitor.py
+++ vnt/network_monitor.py
@@ -1,21 +1,42 @@
 import psutil
 from time import sleep
+import requests
 
-dados_rede_inicial = psutil.net_io_counters()
-downloads_inicial = dados_rede_inicial.bytes_recv
-upload_inicial = dados_rede_inicial.bytes_sent
 
-sleep(1)
+def monitor():
+    dados_rede_inicial = psutil.net_io_counters()
+    downloads_inicial = dados_rede_inicial.bytes_recv
+    upload_inicial = dados_rede_inicial.bytes_sent
 
-dados_rede_final = psutil.net_io_counters()
-downloads_final = dados_rede_final.bytes_recv
-upload_final = dados_rede_final.bytes_sent
+    sleep(1)
 
-MegabytesD = downloads_final - downloads_inicial
-MegabytesU = upload_final - upload_inicial
+    dados_rede_final = psutil.net_io_counters()
+    downloads_final = dados_rede_final.bytes_recv
+    upload_final = dados_rede_final.bytes_sent
 
-Mbps_D = (MegabytesD / 1048576) * 8
-Mbps_U = (MegabytesU / 1048576) * 8
+    MegabytesD = downloads_final - downloads_inicial
+    MegabytesU = upload_final - upload_inicial
 
-print(f"Download:{Mbps_D:.2f} Mb/s")
-print(f"upload: {Mbps_U:.2f} Mb/s")
+    Mbps_D = (MegabytesD / 1048576) * 8
+    Mbps_U = (MegabytesU / 1048576) * 8
+
+    print(f"Download:{Mbps_D:.2f} Mb/s")
+    print(f"upload: {Mbps_U:.2f} Mb/s")
+
+def obter_ip_publico():
+    try:
+        resposta = requests.get('https://api.ipify.org')
+        if resposta.status_code == 200:
+            return resposta.text
+        else:
+            print(f"Erro ao obter endereço público: {resposta.status_code}")
+            return None
+    except requests.RequestException as e:
+        print(f"Erro ao conectar ao serviço de IP: {e}")
+        return None
+
+ip_publico = obter_ip_publico()
+if ip_publico: 
+    print(f"Endereço IP Público: {ip_publico}")
+else:
+    print("Não foi possível obter o endereço de IP público.")
